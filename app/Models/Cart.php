676b9b903bba47c1401a68d7b71cfdadd7172b08
dimas <dimaspradipto@gmail.com>
2023-06-10 20:29:04 +0700

    list cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,15 +9,20 @@ class Cart extends Model
 {
     use HasFactory;
 
-    protected $guarded =[];
-
-    public function product()
-    {
-        return $this->hasOne(Product::class, 'id', 'products_id');
-    }
+    protected $fillable=[
+        'users_id',
+        'products_id',
+    ];
+    
 
     public function user()
     {
         return $this->belongsTo(User::class, 'users_id', 'id');
     }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+
 }
--- resources/views/pages/frontend/cart.blade.php
+++ resources/views/pages/frontend/cart.blade.php
@@ -34,35 +34,26 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td class="cart-pic first-row">
-                      <img src="{{ asset('/frontend/img/cart-page/product-1.jpg') }}" />
-                    </td>
-                    <td class="cart-title first-row text-center">
-                      <h5>Pure Pineapple</h5>
-                    </td>
-                    <td class="p-price first-row">$60.00</td>
-                    <td class="delete-item"><a href="#"><i class="material-icons">
-                          close
-                        </i></a></td>
-                  </tr>
-                  <tr>
-                    <td class="cart-pic first-row">
-                      <img src="img/cart-page/product-1.jpg" />
-                    </td>
-                    <td class="cart-title first-row text-center">
-                      <h5>Pure Pineapple</h5>
-                    </td>
-                    <td class="p-price first-row">$60.00</td>
-                    <td class="delete-item"><a href="#"><i class="material-icons">
-                          close
-                        </i></a></td>
-                  </tr>
+                  @foreach ($carts as $item)
+                    <tr>
+                      <td class="cart-pic first-row">
+                        <img src="{{ $item->product->galleries()->exists() ? Storage::url($item->product->galleries->first()->url) : 'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' }}" />
+                      </td>
+                      <td class="cart-title first-row text-center">
+                        <h5>{{ $item->product->name }}</h5>
+                      </td>
+                      <td class="p-price first-row">IDR {{ number_format($item->product->price) }}</td>
+                      <td class="delete-item"><a href="#"><i class="material-icons">
+                            close
+                          </i></a></td>
+                    </tr>
+                  @endforeach
+                  
                 </tbody>
               </table>
             </div>
           </div>
-          <div class="col-lg-8">
+          {{-- <div class="col-lg-8">
             <h4 class="mb-4">
               Informasi Pembeli:
             </h4>
@@ -89,21 +80,47 @@
                 </div>
               </form>
             </div>
-          </div>
+          </div> --}}
+
         </div>
       </div>
       <div class="col-lg-4">
         <div class="row">
           <div class="col-lg-12">
             <div class="proceed-checkout">
+              <h4 class="mb-4">
+                Informasi Pembeli:
+              </h4>
               <ul>
-                <li class="subtotal">ID Transaction <span>#SH12000</span></li>
+              <form>
+                <div class="form-group">
+                  <label for="namaLengkap">Nama lengkap</label>
+                  <input type="text" name="name" class="form-control" id="namaLengkap" aria-describedby="namaHelp"
+                    placeholder="Masukan Nama">
+                </div>
+                <div class="form-group">
+                  <label for="namaLengkap">Email Address</label>
+                  <input type="email" name="email" class="form-control" id="emailAddress" aria-describedby="emailHelp"
+                    placeholder="Masukan Email">
+                </div>
+                <div class="form-group">
+                  <label for="alamatLengkap">Alamat Lengkap</label>
+                  <textarea name="address	" class="form-control" id="alamatLengkap" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="namaLengkap">No. HP</label>
+                  <input type="number" name="phone" class="form-control" id="noHP" aria-describedby="noHPHelp"
+                    placeholder="Masukan No. HP">
+                </div>
+              </form>
+              
+                {{-- <li class="subtotal">ID Transaction <span>#SH12000</span></li>
                 <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
                 <li class="subtotal mt-3">Pajak <span>10%</span></li>
                 <li class="subtotal mt-3">Total Biaya <span>$440.00</span></li>
                 <li class="subtotal mt-3">Bank Transfer <span>Mandiri</span></li>
                 <li class="subtotal mt-3">No. Rekening <span>2208 1996 1403</span></li>
-                <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li>
+                <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li> --}}
               </ul>
               <a href="success.html" class="proceed-btn">I ALREADY PAID</a>
             </div>
